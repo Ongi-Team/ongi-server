87583eaca30a598eb0461548a0c80fc8c3313eb3
pooreumjung <pooreumjung02@naver.com>
2026-03-09 20:12:00 +0900

    refactor: CI.yml 수정

--- src/test/java/com/ssu/ongi/OngiApplicationTests.java
+++ src/test/java/com/ssu/ongi/OngiApplicationTests.java
@@ -11,5 +11,4 @@ class OngiApplicationTests {
     @Test
     void contextLoads() {
     }
-
 }
---
+++ src/test/java/com/ssu/ongi/common/config/EmbeddedRedisConfig.java
@@ -0,0 +1,7 @@
+package com.ssu.ongi.common.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration
+public class EmbeddedRedisConfig {
+}
