0dc5ec26f03b172616facd68c9180a8ebb0eab28
Eunseo Song <162149585+eunseo9311@users.noreply.github.com>
2026-04-06 08:50:20 +0900

    [Refactor] #9 LockTimeRangeResponse 필드명에 at 접미사 추가

--- src/main/java/com/ssu/ongi/domain/medicine/dto/response/LockTimeRangeResponse.java
+++ src/main/java/com/ssu/ongi/domain/medicine/dto/response/LockTimeRangeResponse.java
@@ -3,7 +3,7 @@ package com.ssu.ongi.domain.medicine.dto.response;
 import java.time.LocalTime;
 
 public record LockTimeRangeResponse(
-        LocalTime lockStart,
-        LocalTime lockEnd
+        LocalTime lockStartAt,
+        LocalTime lockEndAt
 ) {
 }
