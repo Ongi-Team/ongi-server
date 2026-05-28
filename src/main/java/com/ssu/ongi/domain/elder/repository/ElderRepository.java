9eff29ec412953043587668a3dcfda0ecd7d8f88
pooreumjung <pooreumjung02@naver.com>
2026-05-29 00:39:19 +0900

    feat: 어르신 FCM 토큰 조회 메서드 추가

--- src/main/java/com/ssu/ongi/domain/elder/repository/ElderRepository.java
+++ src/main/java/com/ssu/ongi/domain/elder/repository/ElderRepository.java
@@ -12,4 +12,7 @@ public interface ElderRepository extends JpaRepository<Elder, Long> {
     Optional<Elder> findByIdAndMemberId(Long elderId, Long memberId);
 
     Optional<Elder> findFirstByMemberId(Long memberId);
+
+    /** FCM 토큰으로 어르신을 조회합니다. 토큰 무효화 시 사용합니다. */
+    Optional<Elder> findByFcmToken(String fcmToken);
 }
