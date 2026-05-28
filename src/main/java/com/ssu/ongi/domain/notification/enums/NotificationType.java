9f831b5a10308c2c01764a24b9115567348d29ad
pooreumjung <pooreumjung02@naver.com>
2026-05-29 00:59:32 +0900

    feat: 복약 시간 알림 타입 추가

--- src/main/java/com/ssu/ongi/domain/notification/enums/NotificationType.java
+++ src/main/java/com/ssu/ongi/domain/notification/enums/NotificationType.java
@@ -3,6 +3,7 @@ package com.ssu.ongi.domain.notification.enums;
 public enum NotificationType {
     MEDICATION_TAKEN,
     MEDICATION_MISSED,
+    MEDICATION_REMINDER,
     DEVICE_TEMP_OFFLINE,
     DEVICE_LONG_OFFLINE
 }
